d1c7990a6267f853b66246aad819a9a32bd25499
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2022-07-22 15:52:17 +0200

    get working connection to myo

--- app/src/main/java/com/tech7fox/myoassistant/FirstFragment.kt
+++ app/src/main/java/com/tech7fox/myoassistant/FirstFragment.kt
@@ -1,28 +1,50 @@
 package com.tech7fox.myoassistant
 
+import android.Manifest
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.tech7fox.myoassistant.databinding.FragmentFirstBinding
+import com.tech7fox.myolink.BaseMyo
+import com.tech7fox.myolink.BaseMyo.ConnectionListener
+import com.tech7fox.myolink.Myo
+import com.tech7fox.myolink.MyoCmds
+import com.tech7fox.myolink.MyoConnector
+import com.tech7fox.myolink.tools.Logy
+
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), ConnectionListener {
 
     private var _binding: FragmentFirstBinding? = null
+    private var mScanning = false
+    private var myMyo: Myo? = null;
+    //private var mScannerCallback: MyoConnector.ScannerCallback = MyoConnector.ScannerCallback {
+    //    Logy.d("log", "test");
+    //}
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+
+        Logy.sLoglevel = Logy.VERBOSE;
+
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+            1
+        )
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
@@ -32,13 +54,48 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.buttonSecond.setOnClickListener {
+            myMyo?.writeVibrate(MyoCmds.VibrateType.SHORT, null)
+            myMyo?.writeLEDs(Color.valueOf(0f, 0f, 255f), Color.valueOf(0f, 0f, 255f), null)
         }
+    }
+
+    override fun onResume() {
+        Logy.d("onResume", "triggered!");
+        val mMyoConnector = MyoConnector(requireContext());
+        mMyoConnector.scan(5000, MyoConnector.ScannerCallback() {
+            Logy.d("scannerCallback", "MYOS:" + it.size);
+            var myo: Myo = it[0];
+            myMyo = myo;
+
+            myo.addConnectionListener(this);
+            myo.connect();
+            myo.writeUnlock(
+                MyoCmds.UnlockType.HOLD
+            ) {
+                myo, _ -> myo.writeVibrate(MyoCmds.VibrateType.LONG, null)
+
+                myo.writeLEDs(
+                    Color.valueOf(0f, 0f, 255f),
+                    Color.valueOf(0f, 255f, 0f),
+                    null
+                );
+            }
+        });
+        super.onResume()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onConnectionStateChanged(myo: BaseMyo, state: BaseMyo.ConnectionState) {
+        Logy.d("ConnectionStateChanged", "Connection changed to: $state");
+        if (view == null) return
+
+        //if (state == BaseMyo.ConnectionState.DISCONNECTED) {
+        //    requireView().post { mContainer.removeView(mMyoViewMap.get(myo)) }
+        //}
     }
 }
